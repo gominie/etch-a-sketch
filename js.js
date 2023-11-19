e0ccc9e3e5dfac5fe06526e135d282970c397082
Gominie <109306648+gominie@users.noreply.github.com>
2023-11-19 17:17:32 +0000

    added color changing effect with js

--- js.js
+++ js.js
@@ -1,11 +1,19 @@
-
-function makeDiv () {
-    const container = document.getElementById('container')
-    for (let i = 1; i <= 256; i++ ) {
-        const div = document.createElement("div");
-        div.className = "grid";
-        container.appendChild(div)
-    }
+function makeDiv() {
+  const container = document.getElementById("container");
+  for (let i = 1; i <= 256; i++) {
+    const grid = document.createElement("div");
+    grid.className = "grid";
+    container.appendChild(grid);
+  }
 }
 
 makeDiv();
+
+const grids = document.querySelectorAll(".grid");
+
+grids.forEach((grid) => {
+  grid.addEventListener("mouseover", () => {
+    console.log("hover triggered")
+    grid.style.backgroundColor = "red";
+  });
+});
